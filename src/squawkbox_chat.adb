afa35c26f4c6ea818d96869e11c42638e9244216
rococo <247701186+baroquelogic@users.noreply.github.com>
2026-09-24 16:43:24 -0500

    Replace Firebase chat with Ada WebSocket broadcast

--- src/squawkbox_chat.adb
+++ src/squawkbox_chat.adb
@@ -1,9 +1,12 @@
+with Ada.Containers.Vectors;
 with Ada.Text_IO;
 
 with AWS.Net.WebSocket.Registry.Control;
 with AWS.Status;
 
 package body Squawkbox_Chat is
+   use type AWS.Net.WebSocket.UID;
+
    type Chat_Socket is new AWS.Net.WebSocket.Object with null record;
 
    overriding procedure On_Open
@@ -12,9 +15,51 @@ package body Squawkbox_Chat is
    overriding procedure On_Message
      (Socket : in out Chat_Socket; Message : String);
 
+   overriding procedure On_Close
+     (Socket : in out Chat_Socket; Message : String);
+
+   overriding procedure On_Error
+     (Socket : in out Chat_Socket; Message : String);
+
+   package Socket_Vectors is new Ada.Containers.Vectors
+     (Index_Type   => Natural,
+      Element_Type => Chat_Socket);
+
+   protected Client_List is
+      procedure Add (Socket : Chat_Socket);
+      procedure Remove (Socket : Chat_Socket);
+      function Snapshot return Socket_Vectors.Vector;
+   private
+      Clients : Socket_Vectors.Vector;
+   end Client_List;
+
    function Create
      (Socket  : AWS.Net.Socket_Access;
       Request : AWS.Status.Data) return AWS.Net.WebSocket.Object'Class;
+
+   protected body Client_List is
+      procedure Add (Socket : Chat_Socket) is
+      begin
+         Clients.Append (Socket);
+      end Add;
+
+      procedure Remove (Socket : Chat_Socket) is
+         Remaining : Socket_Vectors.Vector;
+      begin
+         for Client of Clients loop
+            if Client.Get_UID /= Socket.Get_UID then
+               Remaining.Append (Client);
+            end if;
+         end loop;
+
+         Clients := Remaining;
+      end Remove;
+
+      function Snapshot return Socket_Vectors.Vector is
+      begin
+         return Clients;
+      end Snapshot;
+   end Client_List;
 
    function Create
      (Socket  : AWS.Net.Socket_Access;
@@ -27,12 +72,43 @@ package body Squawkbox_Chat is
          with null record);
    end Create;
 
+   overriding procedure On_Close
+     (Socket : in out Chat_Socket; Message : String)
+   is
+      pragma Unreferenced (Message);
+   begin
+      Client_List.Remove (Socket);
+   end On_Close;
+
+   overriding procedure On_Error
+     (Socket : in out Chat_Socket; Message : String)
+   is
+      pragma Unreferenced (Message);
+   begin
+      Client_List.Remove (Socket);
+   end On_Error;
+
    overriding procedure On_Message
      (Socket : in out Chat_Socket; Message : String)
    is
+      Clients   : constant Socket_Vectors.Vector := Client_List.Snapshot;
+      Sender_Id : constant AWS.Net.WebSocket.UID := Socket.Get_UID;
    begin
       Ada.Text_IO.Put_Line ("CHAT RECEIVED: " & Message);
       Socket.Send (Message);
+
+      for Stored_Client of Clients loop
+         if Stored_Client.Get_UID /= Sender_Id then
+            declare
+               Client : Chat_Socket := Stored_Client;
+            begin
+               AWS.Net.WebSocket.Registry.Send
+                 (Socket  => Client,
+                  Message => Message,
+                  Timeout => 2.0);
+            end;
+         end if;
+      end loop;
    end On_Message;
 
    overriding procedure On_Open
@@ -40,6 +116,7 @@ package body Squawkbox_Chat is
    is
       pragma Unreferenced (Message);
    begin
+      Client_List.Add (Socket);
       Ada.Text_IO.Put_Line ("CHAT: CLIENT CONNECTED");
       Socket.Send ("SQUAWKBOX LINK ESTABLISHED");
    end On_Open;
